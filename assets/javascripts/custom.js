97447a7c0c54313a8766e9618448a45e6628dc82
Tobias Pape <tobias@netshed.de>
2015-06-20 15:23:23 +0200

    dont target=_blank mailto's

--- assets/javascripts/custom.js
+++ assets/javascripts/custom.js
@@ -21,7 +21,7 @@ function gestureStart() {
   for (var i = 0, linksLength = links.length; i < linksLength; i++) {
     // can also be
     //  links[i].hostname != 'subdomain.example.com'
-    if (links[i].hostname != window.location.hostname) {
+    if (links[i].protocol != "mailto:" && links[i].hostname != window.location.hostname) {
       links[i].target = '_blank';
       links[i].className += ' externalLink';
     }
